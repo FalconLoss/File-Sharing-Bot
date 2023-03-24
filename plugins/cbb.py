842459c1004d86d1c5d0e08f540bd823e3e93a0f
Fâlçøñ <89666694+FalconLoss@users.noreply.github.com>
2023-03-24 20:31:37 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : @Zebuleon</a>\n○ Channel : @Pornhwas\n○ Support Group : @Culture_Group</b>",
+            text = f"<b>○ Creator : @Zebuleon</a>\n○ Main Channel : @Pornhwas\n○ Chat Group : @Culture_Group</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
